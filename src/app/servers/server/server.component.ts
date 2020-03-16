0a8c36d8faa74d6920f21e02362060527e519182
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-16 22:09:27 +0530

    Commented app-server It gave error because we didnt pass any parameter to ir Removed workaround for app-server

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -14,11 +14,12 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    let currentId = this.route.snapshot.params['id'];
+    // + sign can be added which converts the fetched '1' to a number
+    let currentId = +this.route.snapshot.params['id'];
     // If is to simply fix routing without params i.e to fix below situation
-    // Servers component has a <app-edit-server> which takes no param 
-    if(currentId==null)
-      currentId = 1;
+    // Servers component has a <app-server> which takes no param 
+    // if(currentId==null)
+    //   currentId = 1;
     this.server = this.serversService.getServer(currentId);
   }
 
