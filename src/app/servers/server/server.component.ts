f2cd9c9f994ca7a64fe33302eb2dfde634180857
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-16 22:02:53 +0530

    Fetech server data using ActivatedRoute

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,15 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    let currentId = this.route.snapshot.params['id'];
+    // If is to simply fix routing without params i.e to fix below situation
+    // Servers component has a <app-edit-server> which takes no param 
+    if(currentId==null)
+      currentId = 1;
+    this.server = this.serversService.getServer(currentId);
   }
 
 }
