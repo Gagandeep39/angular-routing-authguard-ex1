fbcd99ff93fbf8205d5a0409e3f7a0410763f458
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-16 23:27:44 +0530

    Created separate Router File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,20 +13,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes : Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent },  // 2 parameter
-  ] },
-  // // { path: 'users/:id', component: UserComponent }, // 1 parameter
-  // { path: 'users/:id/:name', component: UserComponent },  // 2 parameter
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent },
-  ] },
-  {path: '**', component: PageNotFoundComponent}  // '**' implies all paths
-];  //Parsed from top to bottom
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -42,7 +29,7 @@ const appRoutes : Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
