a54f806f1584640132a15e434ec58001e7a380b9
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-16 22:31:59 +0530

    Added nested components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,10 @@ const appRoutes : Routes = [
   { path: 'users', component: UsersComponent },
   // { path: 'users/:id', component: UserComponent }, // 1 parameter
   { path: 'users/:id/:name', component: UserComponent },  // 2 parameter
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id', component: ServerComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent },
+  { path: 'servers', component: ServersComponent, children: [
+    { path: ':id', component: ServerComponent },
+    { path: ':id/edit', component: EditServerComponent },
+  ] },
 ];
 
 @NgModule({
