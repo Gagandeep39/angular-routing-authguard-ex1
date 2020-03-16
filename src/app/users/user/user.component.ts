1065b5b07d364587cbf23653a002562c7bea799a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-16 20:46:07 +0530

    Added a method to reach path from within path Used subscription() for params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -9,13 +9,26 @@ import { ActivatedRoute } from '@angular/router';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     }
+
+    // Asynchrnous fetches parameter
+    // Works ven when same componetn(path) is called from within 
+    this.route.params.subscribe((data: Params)=>{
+      this.user = {
+        id: data['id'],
+        name: data['name']
+      }
+    })
+  }
+
+  reloadData(){
+    this.router.navigate(['users', 12, 'Gagan'])
   }
 
 }
