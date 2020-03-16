26517a5d4bcf7483facd613df8e5080d479923a1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-16 23:10:00 +0530

    Edit Server component now loads dynamic data Edit server content displayed when Allowed Edit is true Added a button in server component to edit Added a route in srevercomponent that preserve previous query Removed app edit server tag from serverlist

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,11 +12,21 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+
+    let serverId : number;
+    this.route.params.subscribe((params: Params)=>{
+      serverId = params['id'];
+    })
+    this.route.queryParams.subscribe((params: Params)=>{
+      this.allowEdit = params['allowEdit']=='1'? true : false;
+    })
+
+    this.server = this.serversService.getServer(serverId);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
 
