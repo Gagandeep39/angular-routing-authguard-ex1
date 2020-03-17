c4b8b0860da6d34fce7daf2d3d9db61d96da3312
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-17 10:23:53 +0530

    Removed an unnecessary import

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { stat } from 'fs';
 
 @Component({
   selector: 'app-home',
